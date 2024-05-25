8109ba4094fd3f2c38d43f1b3d96d028a5a7c984
Waisoon123 <waisoon1999@gmail.com>
2024-05-26 01:20:33 +0800

    Merge branch 'main' of https://github.com/MarcusLimJX/hacksingapore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { NavBar } from "./navbar";
+import { LandingHero } from "./LandingHero";
 import { Avatar } from "./avatar";
 
 function App() {
 	return (
 		<div>
 			<NavBar />
+			<LandingHero />
 			<Avatar />
 		</div>
 	);
