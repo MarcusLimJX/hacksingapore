54dc319775a53a0a7e786049bdeacb94380fa98f
Waisoon123 <waisoon1999@gmail.com>
2024-05-26 02:01:59 +0800

    Update main.jsx
    Remove duplication

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Finance from './Finance.jsx'
-import Health from './Health.jsx'
-import Login from './Login.jsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Finance from "./Finance.jsx";
+import Health from "./Health.jsx";
+import Login from "./Login.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        children: [
-          { path: "/", element: <App /> },
-          { path: "/finance", element: <Finance /> },
-          { path: "/health", element: <Health /> },
-          { path: "/login", element: <Login /> },
-        ],
-      },
-    ],
-  },
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				children: [
+					{ path: "/finance", element: <Finance /> },
+					{ path: "/health", element: <Health /> },
+					{ path: "/login", element: <Login /> },
+				],
+			},
+		],
+	},
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<RouterProvider router={router} />
+);
